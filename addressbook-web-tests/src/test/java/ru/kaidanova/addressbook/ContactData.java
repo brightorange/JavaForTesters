9dd9a2201e0d7c568c9f96c752160d5f87d3b6cc
Inga <inga-loputneva@yandex.ru>
2016-05-28 23:09:12 +0200

    Contact creation tests + group creation tests
    Добавлены и отрефакторены тесты для создания группы и для создания
    контакта

---
+++ addressbook-web-tests/src/test/java/ru/kaidanova/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.kaidanova.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String secondname;
+    private final String title;
+    private final String address;
+    private final String mobile;
+
+    public ContactData(String firstname, String secondname, String title, String address, String mobile) {
+        this.firstname = firstname;
+        this.secondname = secondname;
+        this.title = title;
+        this.address = address;
+        this.mobile = mobile;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+}

731bf76c6b0ef2df134f4b2863cea87a487ce18d
inga <inga-loputneva@yandex.ru>
2016-07-08 13:43:06 +0200

    Добавлен генератор тестовых данных ля контактов

---
+++ addressbook-web-tests/src/test/java/ru/kaidanova/addressbook/generator/ContactDataGenerator.java
@@ -0,0 +1,87 @@
+package ru.kaidanova.addressbook.generator;
+
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.kaidanova.addressbook.model.ContactData;
+import ru.kaidanova.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "target file")
+    public String file;
+
+    @Parameter(names = "-d", description = "data format")
+    public String format;
+
+    public static  void main(String args[]) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    public void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if (format.equals("xml")) {
+            saveAsXml(contacts, new File(file));
+        } else if (format.equals("json")) {
+            saveAsJson(contacts, new File(file));
+        } else {
+            System.out.println("Wrong file format " + format);
+        }
+    }
+
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+    }
+
+    private void saveAsXml(List<ContactData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+
+
+    private List<ContactData> generateContacts(int count) {
+                   List<ContactData> contacts = new ArrayList<ContactData>();
+        File photo = new File("src/test/resources/Screenshot_1.png");
+            for (int i = 0; i < count; i++) {
+                contacts.add(new ContactData().withFirstName(String.format("test %s", i)).withSecondName(String.format("test %s", i))
+                        .withAddress(String.format("test %s", i)).withMobile(String.format("test %s", i))
+                        .withPhoto(photo).withGroup("test1"));
+            }
+            return contacts;
+        }
+    }
+
+
+

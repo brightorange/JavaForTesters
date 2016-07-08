51df7c4fc41434e72ef06f0e57dbdf2d55a539ca
inga <inga-loputneva@yandex.ru>
2016-07-08 15:26:03 +0200

    Создан провайдер тестовых данных для создания контакта

--- addressbook-web-tests/src/test/java/ru/kaidanova/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/kaidanova/addressbook/tests/ContactCreationTests.java
@@ -1,22 +1,48 @@
 package ru.kaidanova.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.kaidanova.addressbook.model.ContactData;
 import ru.kaidanova.addressbook.model.Contacts;
+import ru.kaidanova.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+
+    }
+
+
+    @Test(dataProvider = "validContactsFromXml")
+    public void testContactCreation(ContactData contact) {
 
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/Screenshot_1.png");
-        ContactData contact = new ContactData().withFirstName("test1").withSecondName("test2").withAddress("test3").withMobile("test4").withPhoto(photo).withGroup("test1");
+     //   File photo = new File("src/test/resources/Screenshot_1.png");
+     //   ContactData contact = new ContactData().withFirstName("test1").withSecondName("test2").withAddress("test3").withMobile("test4").withPhoto(photo).withGroup("test1");
         app.contact().create(contact);
         app.goTo().homePage();
         Contacts after = app.contact().all();
